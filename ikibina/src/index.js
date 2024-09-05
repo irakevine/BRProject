04e20e69af7de1a7efc902abee8527674e90dcae
“Kevine <ikevine2016@gmail.com>
2024-09-05 14:29:06 +0200

    fixing the data

--- ikibina/src/index.js
+++ ikibina/src/index.js
@@ -10,16 +10,18 @@ const LandingPage = () => {
         <h1>Welcome to My Landing Page</h1>
       </header>
       <section className="hero">
-        <menu
-          style={{
-            display: "flex",
-            flexDirection: "row ",
-            listStyle: "none !important ",
-          }}
-        >
-          <li style={{ listStyle: "none !important" }}>Home</li>
-          <li>Contact</li>
-          <li>Login</li>
+        <menu style={{}}>
+          <ul
+            style={{
+              display: "flex",
+              flexDirection: "row ",
+              listStyle: "none !important ",
+            }}
+          >
+            <li>Home</li>
+            <li>Contact</li>
+            <li>Login</li>
+          </ul>
         </menu>
       </section>
     </React.StrictMode>
