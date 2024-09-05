83dbd9970775ff1075740b0199720be7b4d07d81
“Kevine <ikevine2016@gmail.com>
2024-09-05 10:50:43 +0200

    add menu's link

--- ikibina/src/index.js
+++ ikibina/src/index.js
@@ -6,12 +6,15 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const LandingPage = () => {
   return (
     <React.StrictMode>
-      <header style={{ backgroundColor: "skyblue", fontSize:'10px' }}>
+      <header style={{ backgroundColor: "skyblue", fontSize: "10px" }}>
         <h1>Welcome to My Landing Page</h1>
       </header>
       <section className="hero">
-        <h2>This is a hero section</h2>
-        <p>This is a sample paragraph.</p>
+        <menu>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>Login</li>
+        </menu>
       </section>
     </React.StrictMode>
   );
