8dd79292c09f0fa82e5c6651f984be393e9581c4
“Kevine <ikevine2016@gmail.com>
2024-09-05 10:42:25 +0200

    adding header

--- ikibina/src/index.js
+++ ikibina/src/index.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 const LandingPage = () => {
   return (
     <React.StrictMode>
-      <header>
+      <header style={{ backgroundColor: "skyblue", fontSize:'10px' }}>
         <h1>Welcome to My Landing Page</h1>
       </header>
       <section className="hero">
         <h2>This is a hero section</h2>
         <p>This is a sample paragraph.</p>
       </section>
-      <footer>
-        <p>&copy; 2023 My Landing Page</p>
-      </footer>
     </React.StrictMode>
   );
 };
 root.render(<LandingPage />);
-
