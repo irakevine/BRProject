ecfb4cb603d926dc8a1fca4511d58604e504179d
“Kevine <ikevine2016@gmail.com>
2024-09-09 09:48:20 +0200

    adding App.js file

---
+++ reacProj/src/App.js
@@ -0,0 +1,11 @@
+
+function App(){
+    return(
+        <div>
+            <h1>Key React Concept</h1>
+            <p>selected Key React Concept you should know about </p>
+            
+        </div>
+    );
+
+}
