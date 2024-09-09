f8846be23633c2cff7c1e163ade9b3f892bfc65d
“Kevine <ikevine2016@gmail.com>
2024-09-09 09:56:15 +0200

    adding data

--- reacProj/src/App.js
+++ reacProj/src/App.js
@@ -2,9 +2,16 @@
 function App(){
     return(
         <div>
+            <header>
             <h1>Key React Concept</h1>
             <p>selected Key React Concept you should know about </p>
-            
+            </header>
+           <ul>
+            <li className="concept">
+                <img src="TODO: IMAGE" alt="TODO: TITLE"></img>
+            </li>
+           </ul>
+
         </div>
     );
 
