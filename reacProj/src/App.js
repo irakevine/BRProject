484a39735fbc54948adcfbf69c8b96f4b44eb8af
“Kevine <ikevine2016@gmail.com>
2024-09-09 11:38:37 +0200

    adding the images

--- reacProj/src/App.js
+++ reacProj/src/App.js
@@ -29,21 +29,24 @@ function App() {
         <h1>Key React Concept</h1>
         <p>selected Key React Concept you should know about </p>
       </header>
-      <ul id='concepts'>
+      <ul id="concepts">
         <li className="concept">
-          <img src={concepts[0].image} alt={concepts[0].title}></img>
-          <h2>{concepts[0].title}</h2>
-          <p>{concepts[0].description}</p>
+          <img src={concepts[0].image} alt={concepts[0].title}>
+            <h2>{concepts[0].title}</h2>
+            <p>{concepts[0].description}</p>
+          </img>
         </li>
         <li className="concept">
-          <img src={concepts[1].image} alt={concepts[1].title}></img>
-          <h2>{concepts[1].title}</h2>
-          <p>{concepts[1].description}</p>
+          <img src={concepts[1].image} alt={concepts[1].title}>
+            <h2>{concepts[1].title}</h2>
+            <p>{concepts[1].description}</p>
+          </img>
         </li>
         <li className="concept">
-          <img src={concepts[2].image} alt={concepts[2].title}></img>
-          <h2>{concepts[2].title}</h2>
-          <p>{concepts[2].description}</p>
+          <img src={concepts[2].image} alt={concepts[2].title}>
+            <h2>{concepts[2].title}</h2>
+            <p>{concepts[2].description}</p>
+          </img>
         </li>
       </ul>
     </div>
