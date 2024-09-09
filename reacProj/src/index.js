c5d67ddb1ece12a1dd7e8ace65165474a9f7a777
“Kevine <ikevine2016@gmail.com>
2024-09-09 10:53:15 +0200

    adding dat

--- reacProj/src/index.js
+++ reacProj/src/index.js
@@ -7,24 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const LandingPage = () => {
   return (
     <React.StrictMode>
-      <header style={{ backgroundColor: "skyblue", fontSize: "10px" }}>
-        <h1>Welcome to My Landing Page</h1>
-      </header>
-      <section className="hero">
-        <menu style={{}}>
-          <ul
-            style={{
-              display: "flex",
-              flexDirection: "row ",
-              listStyle: "none !important ",
-            }}
-          >
-            <li>Home</li>
-            <li>Contact</li>
-            <li>Login</li>
-          </ul>
-        </menu>
-      </section>
       <App></App>
     </React.StrictMode>
   );
