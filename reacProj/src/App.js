a4b5265990aaf688513ce2022785777042b49e8b
“Kevine <ikevine2016@gmail.com>
2024-09-09 10:41:34 +0200

    adding more image

--- reacProj/src/App.js
+++ reacProj/src/App.js
@@ -35,6 +35,16 @@ function App() {
           <h2>{concepts[0].title}</h2>
           <p>{concepts[0].description}</p>
         </li>
+        <li className="concept">
+          <img src={concepts[1].image} alt={concepts[1].title}></img>
+          <h2>{concepts[1].title}</h2>
+          <p>{concepts[1].description}</p>
+        </li>
+        <li className="concept">
+          <img src={concepts[2].image} alt={concepts[2].title}></img>
+          <h2>{concepts[2].title}</h2>
+          <p>{concepts[2].description}</p>
+        </li>
       </ul>
     </div>
   );
