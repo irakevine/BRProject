a078bfb5b2ec3cb41332756ff519bb8aea298066
“Kevine <ikevine2016@gmail.com>
2024-09-09 11:47:02 +0200

    adding dat

--- reacProj/src/App.js
+++ reacProj/src/App.js
@@ -31,22 +31,23 @@ function App() {
       </header>
       <ul id="concepts">
         <li className="concept">
-          <img src={concepts[0].image} alt={concepts[0].title}>
+          <img src={concepts[0].image} alt={concepts[0].title}> </img>
             <h2>{concepts[0].title}</h2>
             <p>{concepts[0].description}</p>
-          </img>
+         
         </li>
         <li className="concept">
-          <img src={concepts[1].image} alt={concepts[1].title}>
+          <img src={concepts[1].image} alt={concepts[1].title}> </img>
             <h2>{concepts[1].title}</h2>
             <p>{concepts[1].description}</p>
-          </img>
+      
         </li>
         <li className="concept">
-          <img src={concepts[2].image} alt={concepts[2].title}>
+          <img src={concepts[2].image} alt={concepts[2].title}> </img>
+            <h1>hello data</h1>
             <h2>{concepts[2].title}</h2>
             <p>{concepts[2].description}</p>
-          </img>
+         
         </li>
       </ul>
     </div>
