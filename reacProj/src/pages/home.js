fb653dd95f4c25972572c2c3a783f7d9369cac64
“Kevine <ikevine2016@gmail.com>
2024-09-17 11:59:49 +0200

    add react routing in the This Project

--- reacProj/src/pages/home.js
+++ reacProj/src/pages/home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <h1>This is the home page</h1>
+      <Link to="login">click here for see login Page</Link>
     </div>
   );
 }
--- reacProj/src/pages/login.js
+++ reacProj/src/pages/login.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
     <div>
       <h2>this is login Page </h2>
+      <Link to="/">click here for see Home Page</Link>
     </div>
   );
 }
