abb5b9525ee9430819da8dde9a350f58b8feb0c9
“Kevine <ikevine2016@gmail.com>
2024-10-17 08:03:27 +0200

    adding login page

--- reacProj/src/pages/home.js
+++ reacProj/src/pages/home.js
@@ -9,7 +9,7 @@ function Home() {
         click here for see login Page
       </Link>
       <button>Hello There </button>
-      <h1>This is the home page</h1>
+      <h1>This is the login Page</h1>
       <Link to="login" style={{ textDecoration: "none" }}>
         click here for see login Page
       </Link>
